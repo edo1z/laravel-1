a073f254f971c50171cdfe2d748e2b691501ae78
edo1z <endo.yuta@logicky.com>
2024-01-06 15:10:58 +0900

    bladeにレイアウト適用だぜえ

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,10 +1,21 @@
-<form method="POST" action="{{ route('posts.store') }}">
-    @csrf
-    <label for="title">Title:</label>
-    <input type="text" name="title" id="title" required>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-white leading-tight">
+            Post Create
+        </h2>
+    </x-slot>
 
-    <label for="body">Body:</label>
-    <textarea name="body" id="body" required></textarea>
+    <!-- Page Content -->
+    <div class="text-gray-800 dark:text-white" style="padding: 10px;">
+        <form method="POST" action="{{ route('posts.store') }}">
+            @csrf
+            <label for="title">Title:</label>
+            <input type="text" name="title" id="title" required>
 
-    <button type="submit">Submit</button>
-</form>
+            <label for="body">Body:</label>
+            <textarea name="body" id="body" required></textarea>
+
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+</x-app-layout>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,17 @@
-@foreach ($posts as $post)
-<div>
-    <h2>{{ $post->title }}</h2>
-    <p>{{ $post->body }}</p>
-</div>
-@endforeach
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-white leading-tight">
+            My Page Title
+        </h2>
+    </x-slot>
+
+    <!-- Page Content -->
+    <div class="text-gray-800 dark:text-white" style="padding: 10px;">
+        @foreach ($posts as $post)
+        <div>
+            <h2>{{ $post->title }}</h2>
+            <p>{{ $post->body }}</p>
+        </div>
+        @endforeach
+    </div>
+</x-app-layout>
