9aae88934944d9ce8816de299bb4ef1377101cae
edo1z <endo.yuta@logicky.com>
2024-01-06 21:49:05 +0900

    post新規作成画面のデザイン調整だぜえ

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,21 +1,25 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-white leading-tight">
-            Post Create
+            POST新規作成
         </h2>
     </x-slot>
 
     <!-- Page Content -->
-    <div class="text-gray-800 dark:text-white" style="padding: 10px;">
+    <div class="card max-w-2xl mx-auto mt-10 text-gray-800 dark:text-white">
         <form method="POST" action="{{ route('posts.store') }}">
             @csrf
-            <label for="title">Title:</label>
-            <input type="text" name="title" id="title" required>
+            <div>
+                <label for="title" class="label-text">Title</label>
+                <input type="text" name="title" id="title" required class="input input-bordered w-full">
+            </div>
 
-            <label for="body">Body:</label>
-            <textarea name="body" id="body" required></textarea>
+            <div class="mt-5">
+                <label for="body" class="label-text">Body</label>
+                <textarea name="body" id="body" required class="textarea textarea-bordered w-full"></textarea>
+            </div>
 
-            <button type="submit">Submit</button>
+            <button type="submit" class="btn btn-primary mt-5">Submit</button>
         </form>
     </div>
 </x-app-layout>
