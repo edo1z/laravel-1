52d7a5001c897ecf7bf491bdd603e545f7afa9af
edo1z <endo.yuta@logicky.com>
2024-01-06 18:14:24 +0900

    記事一覧画面のテーブルのデザインを修正したんだぜえ

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,13 +7,25 @@
 
     <!-- Page Content -->
     <div class="text-gray-800 dark:text-white p-5">
-        <table class="table-auto">
-            @foreach ($posts as $post)
-            <tr>
-                <td>{{ $post->title }}</td>
-                <td>{{ $post->body }}</td>
-            </tr>
-            @endforeach
+        <a href="{{ route('posts.create') }}" class="btn btn-primary mb-10">新規作成</a>
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Body</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                <tr>
+                    <td>{{ $post->id}}</td>
+                    <td>{{ $post->title }}</td>
+                    <td>{{ $post->body }}</td>
+                </tr>
+                @endforeach
+            </tbody>
         </table>
     </div>
 </x-app-layout>
